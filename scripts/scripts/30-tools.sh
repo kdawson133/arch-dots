d9f2c6e5c5d8b49ac4ebf2ca1e692fbfbe1b61d3
kdawson133 <kdawson133@gmail.com>
2024-06-21 16:57:05 +1000

    updated

--- scripts/scripts/30-tools.sh
+++ scripts/scripts/30-tools.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo pacman -S --needed neovim stow copyq fd zsh waybar rofi alacritty
+sudo pacman -S --needed neovim stow copyq fd zsh waybar rofi alacritty network-manager-applet blueman
